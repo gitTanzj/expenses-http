3d0ad8900aab1171dacc0f887ba77bf976ba7862
gitTanzj <kalle.riit@gmail.com>
2023-09-27 16:00:05 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Mul on liiga palju valjaminekuid</h2>
+      <ExpenseItem/>
+      <ExpenseItem/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../styles/ExpenseItem.css"
+
+export default function ExpenseItem() {
+  return (
+    <h2>Expense</h2>
+  )
+}
