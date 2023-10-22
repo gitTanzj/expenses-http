b68b7ccd858c2346823986d50305d1d9abcbe6c9
gitTanzj <kalle.riit@gmail.com>
2023-10-22 12:35:12 +0300

    ExpensesFilter added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,24 +2,31 @@ import React from 'react'
 import ExpenseItem from './ExpenseItem'
 import "./Expenses.css"
 import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter'
 
 export const Expenses = () => {
 
-    const expenses = [
-        {
-          "date" : new Date(2023, 0, 10),
-          "title" : "New Book",
-          "price" : "30.99"
-        },
-        {
-          "date" : new Date(2023, 0, 10),
-          "title" : "New Jeans",
-          "price" : "35.99"
-        }
-      ]
+  const expenses = [
+      {
+        "date" : new Date(2023, 0, 10),
+        "title" : "New Book",
+        "price" : "30.99"
+      },
+      {
+        "date" : new Date(2023, 0, 10),
+        "title" : "New Jeans",
+        "price" : "35.99"
+      }
+    ]
+
+  const addExpenseFilterHandler = (expenseFilter) => {
+    console.log('In Expenses.js')
+    console.log(expenseFilter)
+  }
 
   return (
     <Card className='expenses'>
+        <ExpensesFilter onAddExpenseFilter={addExpenseFilterHandler}/>
           {expenses.map((ob) => (
             <ExpenseItem date={ob.date} title={ob.title} price={ob.price}/>
           ))}
