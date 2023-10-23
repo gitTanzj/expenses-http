5685709f472e4e880d4baa55c26a58e831b05413
gitTanzj <kalle.riit@gmail.com>
2023-10-23 14:23:48 +0300

    Vormi andmete kuvamine

--- src/App.js
+++ src/App.js
@@ -2,16 +2,40 @@ import './App.css';
 import { Expenses } from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DUMMY_EXPENSES = [
+  {
+    "id" : "e1",
+    "date" : new Date(2023, 0, 10),
+    "title" : "New Book",
+    "price" : "30.99"
+  },
+  {
+    "id" : "e2",
+    "date" : new Date(2023, 0, 10),
+    "title" : "New Jeans",
+    "price" : "35.99"
+  },
+  {
+    "id": "e3",
+    "date": new Date(2023, 9, 23),
+    "title": "New TV",
+    "price": "399.99"
+  }
+]
+
 const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
+
   const addExpenseHandler = (expense) => {
     console.log('In App.js')
     console.log(expense)
+    setExpenses((previousExpenses) => {return [expense, ...previousExpenses]})
   }
 
   return (
     <div className="App">
       <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
-      <Expenses/>
+      <Expenses expenses={expenses}/>
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,25 +4,7 @@ import "./Expenses.css"
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter'
 
-export const Expenses = () => {
-
-  const expenses = [
-      {
-        "date" : new Date(2023, 0, 10),
-        "title" : "New Book",
-        "price" : "30.99"
-      },
-      {
-        "date" : new Date(2023, 0, 10),
-        "title" : "New Jeans",
-        "price" : "35.99"
-      },
-      {
-        "date": new Date(2023, 9, 23),
-        "title": "New TV",
-        "price": "399.99"
-      }
-    ]
+export const Expenses = ({expenses}) => {
 
   const addExpenseFilterHandler = (expenseFilter) => {
     console.log('In Expenses.js')
@@ -33,7 +15,7 @@ export const Expenses = () => {
     <Card className='expenses'>
         <ExpensesFilter onAddExpenseFilter={addExpenseFilterHandler}/>
           {expenses.map((ob) => (
-            <ExpenseItem date={ob.date} title={ob.title} price={ob.price}/>
+            <ExpenseItem date={ob.date} title={ob.title} price={ob.price} id={ob.id}/>
           ))}
     </Card>
   )
